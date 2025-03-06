b9b9f832f65eda4e66743df36f8d77ce615158a4
Chabanois Cédric <cchabanois@gmail.com>
2025-03-06 08:16:18 +0100

    converted  MesFavorisProjectIdManager to lightweight service (#20)
    Some other minor changes

--- src/main/java/mesfavoris/internal/MesFavorisProjectIdManager.kt
+++ src/main/java/mesfavoris/internal/MesFavorisProjectIdManager.kt
@@ -5,19 +5,20 @@ import com.intellij.openapi.project.Project
 import com.intellij.util.io.Ksuid
 import com.intellij.util.xmlb.annotations.Attribute
 
+@Service(Service.Level.PROJECT)
 @State(name = "MesFavorisProjectId", storages = [(Storage(StoragePathMacros.WORKSPACE_FILE))], reportStatistic = false)
 internal class MesFavorisProjectIdManager : SimplePersistentStateComponent<ProjectIdState>(ProjectIdState()) {
     companion object {
-        fun getInstance(project: Project) = project.service<MesFavorisProjectIdManager>();
+        fun getInstance(project: Project) = project.service<MesFavorisProjectIdManager>()
         fun getProjectId(project : Project) : String {
-            val projectIdManager = project.service<MesFavorisProjectIdManager>();
+            val projectIdManager = project.service<MesFavorisProjectIdManager>()
             var projectId = projectIdManager.state.id
             if (projectId == null) {
                 // do not use project name as part of id, to ensure that project dir renaming also will not cause data loss
                 projectId = Ksuid.generate()
                 projectIdManager.state.id = projectId
             }
-            return projectId;
+            return projectId
         }
     }
 }
