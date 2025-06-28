5ed4ba5984c3785d308b0cdec391118fcf232f5d
Chabanois Cédric <cchabanois@gmail.com>
2025-06-28 17:43:50 +0200

    Added JavaEditorBookmarkPropertiesProvider (#67)

---
+++ src/main/java/mesfavoris/internal/extensions/JavaBookmarkTypeExtension.java
@@ -0,0 +1,62 @@
+package mesfavoris.internal.extensions;
+
+import com.intellij.icons.AllIcons;
+import mesfavoris.extensions.AbstractBookmarkTypeExtension;
+import mesfavoris.java.internal.JavaEditorBookmarkPropertiesProvider;
+
+import static mesfavoris.bookmarktype.BookmarkPropertyDescriptor.BookmarkPropertyType.INT;
+import static mesfavoris.bookmarktype.BookmarkPropertyDescriptor.BookmarkPropertyType.STRING;
+import static mesfavoris.bookmarktype.BookmarkPropertyDescriptor.bookmarkPropertyDescriptor;
+import static mesfavoris.java.JavaBookmarkProperties.*;
+
+/**
+ * Bookmark type extension for Java files
+ */
+public class JavaBookmarkTypeExtension extends AbstractBookmarkTypeExtension {
+
+    public static final String BOOKMARK_TYPE_NAME = "java";
+
+    public JavaBookmarkTypeExtension() {
+        super(BOOKMARK_TYPE_NAME, AllIcons.FileTypes.Java);
+
+        // Define properties for Java bookmarks
+        addProperty(bookmarkPropertyDescriptor(PROP_JAVA_ELEMENT_NAME)
+                .type(STRING)
+                .updatable(true)
+                .description("Java element name")
+                .build());
+
+        addProperty(bookmarkPropertyDescriptor(PROP_JAVA_DECLARING_TYPE)
+                .type(STRING)
+                .updatable(true)
+                .description("Declaring type")
+                .build());
+
+        addProperty(bookmarkPropertyDescriptor(PROP_JAVA_ELEMENT_KIND)
+                .type(STRING)
+                .updatable(true)
+                .description("Element kind")
+                .build());
+
+        addProperty(bookmarkPropertyDescriptor(PROP_JAVA_METHOD_SIGNATURE)
+                .type(STRING)
+                .updatable(true)
+                .description("Method signature")
+                .build());
+
+        addProperty(bookmarkPropertyDescriptor(PROP_JAVA_TYPE)
+                .type(STRING)
+                .updatable(true)
+                .description("Java type")
+                .build());
+
+        addProperty(bookmarkPropertyDescriptor(PROP_LINE_NUMBER_INSIDE_ELEMENT)
+                .type(INT)
+                .updatable(true)
+                .description("Line number inside element")
+                .build());
+
+        // Add providers
+        addPropertiesProvider(new JavaEditorBookmarkPropertiesProvider(), 20);
+    }
+}
---
+++ src/main/java/mesfavoris/java/JavaBookmarkProperties.java
@@ -0,0 +1,26 @@
+package mesfavoris.java;
+
+import mesfavoris.model.Bookmark;
+
+public class JavaBookmarkProperties {
+	public static final String PROP_LINE_NUMBER_INSIDE_ELEMENT = "java.lineNumberInsideElement";
+	public static final String PROP_JAVA_ELEMENT_NAME = "java.elementName";
+	public static final String PROP_JAVA_DECLARING_TYPE = "java.declaringType";
+	public static final String PROP_JAVA_ELEMENT_KIND = "java.elementKind";
+	public static final String PROP_JAVA_METHOD_SIGNATURE = "java.methodSignature";
+	public static final String PROP_JAVA_TYPE = "java.type";
+
+	// Element kinds
+	public static final String KIND_FIELD = "field";
+	public static final String KIND_INITIALIZER = "initializer";
+	public static final String KIND_METHOD = "method";
+	public static final String KIND_INTERFACE = "interface";
+	public static final String KIND_ENUM = "enum";
+	public static final String KIND_ANNOTATION = "annotation";
+	public static final String KIND_CLASS = "class";
+	public static final String KIND_TYPE = "type";
+
+	// Common properties
+	public static final String PROPERTY_NAME = Bookmark.PROPERTY_NAME;
+	public static final String PROPERTY_COMMENT = Bookmark.PROPERTY_COMMENT;
+}
