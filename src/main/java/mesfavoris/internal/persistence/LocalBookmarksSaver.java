bd51c8bc3d8ce66eca009a703fbab4c6aa078024
cedric chabanois <cchabanois@gmail.com>
2025-09-23 22:42:16 +0200

    LocalBookmarksSaver now also refresh the corresponding VirtualFile

--- src/main/java/mesfavoris/internal/persistence/LocalBookmarksSaver.java
+++ src/main/java/mesfavoris/internal/persistence/LocalBookmarksSaver.java
@@ -1,35 +1,44 @@
 package mesfavoris.internal.persistence;
 
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.vfs.LocalFileSystem;
+import com.intellij.openapi.vfs.VirtualFile;
+import mesfavoris.model.BookmarksTree;
+import mesfavoris.persistence.IBookmarksTreeSerializer;
+import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import mesfavoris.model.BookmarksTree;
-import mesfavoris.persistence.IBookmarksTreeSerializer;
-
 public class LocalBookmarksSaver {
-	private final File file;
-	private final IBookmarksTreeSerializer bookmarksSerializer;
-	
-	public LocalBookmarksSaver(File file, IBookmarksTreeSerializer bookmarksSerializer) {
-		this.file = file;
-		this.bookmarksSerializer = bookmarksSerializer;
-	}
-	
-	public void saveBookmarks(BookmarksTree bookmarksTree) {
-		FileWriter writer = null;
-		try {
-			writer = new FileWriter(file);
-			bookmarksSerializer.serialize(bookmarksTree,
-					bookmarksTree.getRootFolder().getId(), writer);
-		} catch (IOException e) {
-			if (writer != null) {
-				try {
-					writer.close();
-				} catch (IOException ignored) {
-				}
-			}
-		}
-	}	
-	
+    private static final Logger LOG = Logger.getInstance(LocalBookmarksSaver.class);
+    private final File file;
+    private final IBookmarksTreeSerializer bookmarksSerializer;
+
+    public LocalBookmarksSaver(@NotNull File file, @NotNull IBookmarksTreeSerializer bookmarksSerializer) {
+        this.file = file;
+        this.bookmarksSerializer = bookmarksSerializer;
+    }
+
+    public void saveBookmarks(BookmarksTree bookmarksTree) {
+        try (FileWriter writer = new FileWriter(file)) {
+            bookmarksSerializer.serialize(bookmarksTree,
+                    bookmarksTree.getRootFolder().getId(), writer);
+
+            // Reload the file in any open editors
+            refreshFile();
+        } catch (IOException e) {
+            LOG.error("Failed to save bookmarks", e);
+        }
+    }
+
+    private void refreshFile() {
+        VirtualFile virtualFile = LocalFileSystem.getInstance().findFileByIoFile(file);
+        if (virtualFile != null) {
+            // Refresh VFS to detect file changes
+            virtualFile.refresh(false, false);
+        }
+    }
+
 }
