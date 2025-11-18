b5c2e499199b3812ad6fd917b00d4b47ea27240a
cedric chabanois <cchabanois@gmail.com>
2025-11-18 23:00:33 +0100

    Added "Sort by name" action

---
+++ src/main/java/mesfavoris/internal/service/operations/SortByNameOperation.java
@@ -0,0 +1,67 @@
+package mesfavoris.internal.service.operations;
+
+import mesfavoris.BookmarksException;
+import mesfavoris.model.Bookmark;
+import mesfavoris.model.BookmarkDatabase;
+import mesfavoris.model.BookmarkFolder;
+import mesfavoris.model.BookmarkId;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SortByNameOperation {
+	private final BookmarkDatabase bookmarkDatabase;
+
+	public SortByNameOperation(BookmarkDatabase bookmarkDatabase) {
+		this.bookmarkDatabase = bookmarkDatabase;
+	}
+
+	public void sortByName(BookmarkId bookmarkFolderId) throws BookmarksException {
+		bookmarkDatabase.modify(bookmarksTreeModifier -> {
+			List<Bookmark> children = new ArrayList<>(
+					bookmarksTreeModifier.getCurrentTree().getChildren(bookmarkFolderId));
+			List<BookmarkId> bookmarkIds = children.stream().sorted(new BookmarkComparator())
+					.map(Bookmark::getId).collect(Collectors.toList());
+			bookmarksTreeModifier.move(bookmarkIds, bookmarkFolderId);
+		});
+
+	}
+
+	private static class BookmarkComparator implements Comparator<Bookmark> {
+
+		@Override
+		public int compare(Bookmark b1, Bookmark b2) {
+			String name1 = b1.getPropertyValue(Bookmark.PROPERTY_NAME);
+			String name2 = b2.getPropertyValue(Bookmark.PROPERTY_NAME);
+			if (b1 instanceof BookmarkFolder) {
+				if (b2 instanceof BookmarkFolder) {
+					return compareName(name1, name2);
+				} else {
+					return -1;
+				}
+			}
+			if (b2 instanceof BookmarkFolder) {
+				return 1;
+			}
+			return compareName(name1, name2);
+		}
+
+		private int compareName(String name1, String name2) {
+			if (name1 == null) {
+				if (name2 == null) {
+					return 0;
+				} else {
+					return -1;
+				}
+			}
+			if (name2 == null) {
+				return 1;
+			}
+			return name1.compareToIgnoreCase(name2);
+		}
+
+	}
+
+}
