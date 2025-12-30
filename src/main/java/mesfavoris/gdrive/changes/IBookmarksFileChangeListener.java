1be1a44c833919ae5954c6d2c49be5741d13b331
Chabanois Cédric <cchabanois@gmail.com>
2025-12-30 16:14:56 +0100

    Use MessageBus for BookmarksFileChangeManager (#169)

--- src/main/java/mesfavoris/gdrive/changes/IBookmarksFileChangeListener.java
+++ src/main/java/mesfavoris/gdrive/changes/IBookmarksFileChangeListener.java
@@ -1,10 +1,16 @@
 package mesfavoris.gdrive.changes;
 
 import com.google.api.services.drive.model.Change;
+import com.intellij.util.messages.Topic;
 import mesfavoris.model.BookmarkId;
 
+/**
+ * Listener interface for bookmarks file change events.
+ * Events are published via MessageBus.
+ */
 public interface IBookmarksFileChangeListener {
+	Topic<IBookmarksFileChangeListener> TOPIC = Topic.create("BookmarksFileChangeListener", IBookmarksFileChangeListener.class);
 
-	 void bookmarksFileChanged(BookmarkId bookmarkFolderId, Change change);
-	
+	void bookmarksFileChanged(BookmarkId bookmarkFolderId, Change change);
+
 }
